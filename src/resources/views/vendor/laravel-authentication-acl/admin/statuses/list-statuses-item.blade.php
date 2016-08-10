fb67f6eaf2c0bea67a9b1e8a3aabd2937d16221a
zukizaro <minhduong.zaro@gmail.com>
2016-08-10 16:14:33 +0700

    Fix tasks

--- src/resources/views/vendor/laravel-authentication-acl/admin/statuses/list-statuses-item.blade.php
+++ src/resources/views/vendor/laravel-authentication-acl/admin/statuses/list-statuses-item.blade.php
@@ -1,6 +1,8 @@
 <div class="row margin-bottom-12">
     <div class="col-md-12">
-        <a href="{!! URL::route('groups.edit') !!}" class="btn btn-info pull-right"><i class="fa fa-plus"></i> Add New</a>
+        <a href="{!! URL::route('statuses.edit') !!}" class="btn btn-info pull-right">
+            <i class="fa fa-plus"><?php echo trans('statuses.status_add') ?></i>
+        </a>
     </div>
 </div>
 <?php $statuses = @$data['statuses']; ?>
@@ -9,15 +11,17 @@
 <table class="table table-hover">
     <thead>
         <tr>
-            <th>Group name</th>
-            <th>Operations</th>
+            <th style="width: 5%"><?php echo trans('statuses.status_order') ?></th>
+            <th style="width:70%"><?php echo trans('statuses.status_title') ?></th>
+            <th><?php echo trans('statuses.status_action') ?></th>
         </tr>
     </thead>
     <tbody>
         @foreach($statuses as $status)
         <tr>
-            <td style="width:90%">{!! $status->status_title !!}</td>
-            <td style="width:10%">
+            <td></td>
+            <td>{!! $status->status_title !!}</td>
+            <td>
                 <a href="{!! URL::route('statuses.edit', ['id' => $status->status_id]) !!}"><i class="fa fa-edit fa-2x"></i></a>
                 <a href="{!! URL::route('statuses.delete',['id' => $status->status_id, '_token' => csrf_token()]) !!}" class="margin-left-5 delete"><i class="fa fa-trash-o fa-2x"></i></a>
                 <span class="clearfix"></span>
--- src/resources/views/vendor/laravel-authentication-acl/admin/tasks/list-tasks-item.blade.php
+++ src/resources/views/vendor/laravel-authentication-acl/admin/tasks/list-tasks-item.blade.php
@@ -3,9 +3,9 @@
         <a href="{!! URL::route('tasks.edit') !!}" class="btn btn-info pull-right"><i class="fa fa-plus"></i><?php echo trans('tasks.task_add') ?></a>
     </div>
 </div>
-<?php $posts = @$data['posts']; ?>
+<?php $tasks = @$data['tasks']; ?>
 
-@if( ! $posts->isEmpty() )
+@if( !$tasks->isEmpty() )
 <table class="table table-hover">
     <thead>
         <tr>
@@ -15,43 +15,32 @@
             <th><?php echo trans('tasks.task_action') ?></th>
         </tr>
     </thead>
-    <tbody>
-<<<<<<< HEAD
-        @foreach($posts as $post)
+    <tbody>  
+        <?php
+        $nav = $tasks->toArray();
+        $counter = ($nav['current_page'] - 1) * $nav['per_page'] + 1;
+        ?>
+        @foreach($tasks as $task)
         <tr>
-            <td style="width:90%">{!! $post->post_title !!}</td>
-            <td style="width:10%">
-                <a href="{!! URL::route('posts.edit', ['id' => $post->post_id]) !!}"><i class="fa fa-edit fa-2x"></i></a>
-                <a href="{!! URL::route('posts.delete',['id' => $post->post_id, '_token' => csrf_token()]) !!}" class="margin-left-5 delete"><i class="fa fa-trash-o fa-2x"></i></a>
+            <!--ORDER-->
+            <td><?php echo $counter;
+        $counter++; ?></td>
+
+            <!--TITLE-->
+            <td>{!! $task->task_title !!}</td>
+
+            <!--STATUS-->
+            <td>{!! @$data['statuses'][$task->status_id] !!}</td>
+
+            <!--ACTION-->
+            <td>
+                <a href="{!! URL::route('tasks.edit', ['id' => $task->task_id]) !!}"><i class="fa fa-edit fa-2x"></i></a>
+                <a href="{!! URL::route('tasks.delete',['id' => $task->task_id, '_token' => csrf_token()]) !!}" class="margin-left-5 delete"><i class="fa fa-trash-o fa-2x"></i></a>
                 <span class="clearfix"></span>
             </td>
         </tr>
         @endforeach
-=======
-        <?php
-            $nav = $tasks->toArray();
-            $counter = ($nav['current_page'] - 1) * $nav['per_page'] + 1;
-        ?>
-            @foreach($tasks as $task)
-            <tr>
-                <!--ORDER-->
-                <td><?php echo $counter; $counter++; ?></td>
 
-                <!--TITLE-->
-                <td>{!! $task->task_title !!}</td>
-
-                <!--STATUS-->
-                <td>{!! @$data['statuses'][$task->status_id] !!}</td>
-
-                <!--ACTION-->
-                <td>
-                    <a href="{!! URL::route('tasks.edit', ['id' => $task->task_id]) !!}"><i class="fa fa-edit fa-2x"></i></a>
-                    <a href="{!! URL::route('tasks.delete',['id' => $task->task_id, '_token' => csrf_token()]) !!}" class="margin-left-5 delete"><i class="fa fa-trash-o fa-2x"></i></a>
-                    <span class="clearfix"></span>
-                </td>
-            </tr>
-            @endforeach
->>>>>>> origin/master
     </tbody>
 </table>
 @else
