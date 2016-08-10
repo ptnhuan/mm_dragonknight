2f43418cc9e679fa887ff04287b689694e86addc
Kang <ptnhuan@gmail.com>
2016-08-10 14:18:34 +0700

    Commit fontawesome

--- src/resources/views/vendor/laravel-authentication-acl/admin/layouts/base.blade.php
+++ src/resources/views/vendor/laravel-authentication-acl/admin/layouts/base.blade.php
@@ -14,7 +14,7 @@
     {!! HTML::style('packages/jacopo/laravel-authentication-acl/css/style.css') !!}
     {!! HTML::style('packages/jacopo/laravel-authentication-acl/css/baselayout.css') !!}
     {!! HTML::style('packages/jacopo/laravel-authentication-acl/css/fonts.css') !!}
-    {!! HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css') !!}
+    {!! HTML::style('packages/jacopo/laravel-authentication-acl/css/font-awesome.min.css') !!}
 
     @yield('head_css')
     {{-- End head css --}}
