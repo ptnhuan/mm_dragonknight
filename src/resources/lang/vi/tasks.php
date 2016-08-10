05fdd3db73e5dc9cac17862e8aa98a7d12a8f9e3
Kang <ptnhuan@gmail.com>
2016-08-10 12:20:23 +0700

    Commit lang tasks

--- src/resources/lang/vi/tasks.php
+++ src/resources/lang/vi/tasks.php
@@ -18,12 +18,14 @@ return [
     'task_page_search' => 'Tìm kiếm task',
     'task_search' => 'Tìm kiếm',
     'task_search_title' => 'Tên tiêu đề:',
-    
+
     'task_delete_confirm' => 'Bạn có muốn xóa task này không?',
     'task_add' => 'Thêm mới',
     'task_save' => 'Lưu',
     'task_delete' => 'Xóa',
     'task_edit' => 'Chỉnh sửa',
     'task_create' => 'Tạo mới',
-    
+
+    'task_order' => 'STT',
+
 ];
