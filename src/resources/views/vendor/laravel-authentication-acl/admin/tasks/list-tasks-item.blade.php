3d0376421c314530a2da2bbfa014a83742c1c2b8
zukizaro <minhduong.zaro@gmail.com>
2016-08-10 13:03:10 +0700

    edit list-task-item

--- src/resources/views/vendor/laravel-authentication-acl/admin/tasks/list-tasks-item.blade.php
+++ src/resources/views/vendor/laravel-authentication-acl/admin/tasks/list-tasks-item.blade.php
@@ -10,9 +10,9 @@
     <thead>
         <tr>
             <th style="width: 5%"><?php echo trans('tasks.task_order') ?></th>
-            <th style="width: 35%"><?php echo trans('tasks.task_title') ?></th>
-            <th style="width: 15%"><?php echo trans('tasks.task_status') ?></th>
-            <th><?php echo trans('tasks.task_action') ?></th>
+            <th style="width: 65%"><?php echo trans('tasks.task_title') ?></th>
+            <th style="width: 10%"><?php echo trans('tasks.task_status') ?></th>
+            <th style="width: 10%"><?php echo trans('tasks.task_action') ?></th>
         </tr>
     </thead>
     <tbody>
@@ -23,16 +23,16 @@
             @foreach($tasks as $task)
             <tr>
                 <!--ORDER-->
-                <td><?php echo $counter; $counter++; ?></td>
+                <td style="width: 5%"><?php echo $counter; $counter++; ?></td>
 
                 <!--TITLE-->
-                <td>{!! $task->task_title !!}</td>
+                <td style="width: 35%">{!! $task->task_title !!}</td>
 
                 <!--STATUS-->
-                <td>{!! @$data['statuses'][$task->status_id] !!}</td>
+                <td style="width: 10%">{!! @$data['statuses'][$task->status_id] !!}</td>
 
                 <!--ACTION-->
-                <td>
+                <td style="width: 10%">
                     <a href="{!! URL::route('tasks.edit', ['id' => $task->task_id]) !!}"><i class="fa fa-edit fa-2x"></i></a>
                     <a href="{!! URL::route('tasks.delete',['id' => $task->task_id, '_token' => csrf_token()]) !!}" class="margin-left-5 delete"><i class="fa fa-trash-o fa-2x"></i></a>
                     <span class="clearfix"></span>
