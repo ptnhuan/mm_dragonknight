cadb50ba1962bbf051b305be389a70401b6f582d
Tấn Tú <petertu00@gmail.com>
2016-08-11 18:13:11 +0700

    fix posts
    đã trans

--- src/app/Http/ViewComposers/PostsServiceProvider.php
+++ src/app/Http/ViewComposers/PostsServiceProvider.php
@@ -27,11 +27,11 @@ class PostsServiceProvider extends ServiceProvider {
         //Testimonials
         View::composer(['laravel-authentication-acl::admin.posts.*'], function ($view) {
             $view->with('sidebar_items', [
-                "List" => [
+                trans('posts.post_list') => [
                     "url" => URL::route('posts.list'),
                     "icon" => '<i class="fa fa-list"></i>'
                 ],
-                "Add testimonial" => [
+                trans('posts.post_add') => [
                     "url" => URL::route('posts.edit'),
                     "icon" => '<i class="fa fa-plus"></i>'
                 ]
