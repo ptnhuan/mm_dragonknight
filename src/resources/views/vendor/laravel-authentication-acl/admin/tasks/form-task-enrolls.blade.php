9352fa1ebb2c5770df1afce7d2e71260fd8d5d10
Kang <ptnhuan@gmail.com>
2016-08-13 19:40:10 +0700

    Undone

--- src/resources/views/vendor/laravel-authentication-acl/admin/tasks/form-task-enrolls.blade.php
+++ src/resources/views/vendor/laravel-authentication-acl/admin/tasks/form-task-enrolls.blade.php
@@ -2,7 +2,6 @@
 <div class="form-group">
     {!! Form::label('task_enrolls', trans('tasks.task_enrolls').':') !!}
     {!! Form::text('task_enrolls', null, ['class' => 'form-control', 'placeholder' => trans('tasks.task_enrolls').'']) !!}
-    <b class='user-search'>Search</b>
     <span class="text-danger">{!! $errors->first('task_enrolls') !!}</span>
 </div>
 
@@ -10,13 +9,13 @@
 <div class="user-enrolled">
     <div class="row">
         <!--USER SEARCH-->
-        <div class="col-md-8 user-search-list">
+        <div class="col-md-8 user-search-list" style="display: none">
             <table class="table table-hover">
                 <thead>
                     <tr>
-                        <th style="width: 20%"><?php echo trans('tasks.user_avatar') ?></th>
                         <th style="width: 20%"><?php echo trans('tasks.user_name') ?></th>
                         <th style="width: 15%"><?php echo trans('tasks.user_email') ?></th>
+                        <th style="width: 15%"><?php echo trans('tasks.task_action') ?></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -24,19 +23,23 @@
                 </tbody>
             </table>
         </div>
-        <div class="col-md-1">
 
-        </div>
         <!--USER ENROLLED -->
-        <div class="col-md-3">
+        <div class="col-md-4">
             <table class="table table-hover">
                 <thead>
                     <tr>
-                        <th style="width: 40%"><?php echo trans('tasks.task_title') ?></th>
-                        <th style="width: 15%"><?php echo trans('tasks.task_status') ?></th>
+                        <th style="width: 40%"><?php echo trans('tasks.task_user_name') ?></th>
+                        <th style="width: 15%"><?php echo trans('tasks.task_user_status') ?></th>
+                        <th style="width: 15%"><?php echo trans('tasks.task_action') ?></th>
                     </tr>
                 </thead>
                 <tbody>
+                    <tr>
+                        <td>A</td>
+                        <td>AA</td>
+                        <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -50,6 +53,7 @@
 <script>
     $("#task_enrolls").keypress(function () {
         $('.user-search-list tbody').empty();
+        $('.user-search-list').show();
         $.ajax({
             type: "POST",
             url: "{!!  URL::route('ajax_user.search') !!}",
@@ -59,15 +63,18 @@
             },
             success: function (data) {
                 $('.user-list-search').show();
+                $('.user-search-list tbody').empty();
+                $('.user-search-list').show();
+
                 if (data) {
                     var html_user_item = '';
                     var user_list = jQuery.parseJSON(data);
                     for (i = 0; i < user_list.length; i++) {
                         html_user_item =
                                 '<tr> \n\
-                                            <td>' + user_list[i].id + '</td>  \n\
                                             <td>' + user_list[i].first_name + ' ' + user_list[i].first_name + '</td>  \n\
                                             <td>' + user_list[i].email + '</td>  \n\
+                                            <td>  <a href="#" class="assign-task" ><i class="fa fa-arrow-right" aria-hidden="true"></i></a>   </td>                           \n\
                                         </tr>';
                         $('.user-search-list tbody').append(html_user_item);
                     }
@@ -78,6 +85,10 @@
                 console.log('error');
             }
         });
+    });
+
+    $('.assign-task').click(function(){
+        
     });
 
 </script>
